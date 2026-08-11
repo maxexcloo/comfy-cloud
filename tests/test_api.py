4167ab1a22e0f73ac91ba9a38cc331abebd0809c
Max Schaefer <max@excloo.com>
2026-08-11 11:42:38 +1000

    Declare Comfy Control as first-party

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@ from unittest.mock import AsyncMock
 
 import httpx
 import pytest
+
 from comfy_control.comfy import OutputRef
 from comfy_control.config import Settings
 from comfy_control.worker import create_app as create_gateway
